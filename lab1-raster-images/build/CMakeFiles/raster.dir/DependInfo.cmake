4baa053aaa4bad2e27c60cf34ff7df03f8ad3d5b
Zichun Chong <Zichun.Chong@ibm.com>
2021-02-09 20:50:43 -0500

    Finish Lab 1 Raster Images

---
+++ lab1-raster-images/build/CMakeFiles/raster.dir/DependInfo.cmake
@@ -0,0 +1,35 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/Zichun.Chong@ibm.com/Desktop/csc317/computer-graphics-raster-images/main.cpp" "/Users/Zichun.Chong@ibm.com/Desktop/csc317/computer-graphics-raster-images/build/CMakeFiles/raster.dir/main.cpp.o"
+  "/Users/Zichun.Chong@ibm.com/Desktop/csc317/computer-graphics-raster-images/src/demosaic.cpp" "/Users/Zichun.Chong@ibm.com/Desktop/csc317/computer-graphics-raster-images/build/CMakeFiles/raster.dir/src/demosaic.cpp.o"
+  "/Users/Zichun.Chong@ibm.com/Desktop/csc317/computer-graphics-raster-images/src/desaturate.cpp" "/Users/Zichun.Chong@ibm.com/Desktop/csc317/computer-graphics-raster-images/build/CMakeFiles/raster.dir/src/desaturate.cpp.o"
+  "/Users/Zichun.Chong@ibm.com/Desktop/csc317/computer-graphics-raster-images/src/hsv_to_rgb.cpp" "/Users/Zichun.Chong@ibm.com/Desktop/csc317/computer-graphics-raster-images/build/CMakeFiles/raster.dir/src/hsv_to_rgb.cpp.o"
+  "/Users/Zichun.Chong@ibm.com/Desktop/csc317/computer-graphics-raster-images/src/hue_shift.cpp" "/Users/Zichun.Chong@ibm.com/Desktop/csc317/computer-graphics-raster-images/build/CMakeFiles/raster.dir/src/hue_shift.cpp.o"
+  "/Users/Zichun.Chong@ibm.com/Desktop/csc317/computer-graphics-raster-images/src/over.cpp" "/Users/Zichun.Chong@ibm.com/Desktop/csc317/computer-graphics-raster-images/build/CMakeFiles/raster.dir/src/over.cpp.o"
+  "/Users/Zichun.Chong@ibm.com/Desktop/csc317/computer-graphics-raster-images/src/reflect.cpp" "/Users/Zichun.Chong@ibm.com/Desktop/csc317/computer-graphics-raster-images/build/CMakeFiles/raster.dir/src/reflect.cpp.o"
+  "/Users/Zichun.Chong@ibm.com/Desktop/csc317/computer-graphics-raster-images/src/rgb_to_gray.cpp" "/Users/Zichun.Chong@ibm.com/Desktop/csc317/computer-graphics-raster-images/build/CMakeFiles/raster.dir/src/rgb_to_gray.cpp.o"
+  "/Users/Zichun.Chong@ibm.com/Desktop/csc317/computer-graphics-raster-images/src/rgb_to_hsv.cpp" "/Users/Zichun.Chong@ibm.com/Desktop/csc317/computer-graphics-raster-images/build/CMakeFiles/raster.dir/src/rgb_to_hsv.cpp.o"
+  "/Users/Zichun.Chong@ibm.com/Desktop/csc317/computer-graphics-raster-images/src/rgba_to_rgb.cpp" "/Users/Zichun.Chong@ibm.com/Desktop/csc317/computer-graphics-raster-images/build/CMakeFiles/raster.dir/src/rgba_to_rgb.cpp.o"
+  "/Users/Zichun.Chong@ibm.com/Desktop/csc317/computer-graphics-raster-images/src/rotate.cpp" "/Users/Zichun.Chong@ibm.com/Desktop/csc317/computer-graphics-raster-images/build/CMakeFiles/raster.dir/src/rotate.cpp.o"
+  "/Users/Zichun.Chong@ibm.com/Desktop/csc317/computer-graphics-raster-images/src/simulate_bayer_mosaic.cpp" "/Users/Zichun.Chong@ibm.com/Desktop/csc317/computer-graphics-raster-images/build/CMakeFiles/raster.dir/src/simulate_bayer_mosaic.cpp.o"
+  "/Users/Zichun.Chong@ibm.com/Desktop/csc317/computer-graphics-raster-images/src/write_ppm.cpp" "/Users/Zichun.Chong@ibm.com/Desktop/csc317/computer-graphics-raster-images/build/CMakeFiles/raster.dir/src/write_ppm.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  "../json"
+  "../shared/json"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ lab1-raster-images/build/CMakeFiles/raster.dir/cmake_clean.cmake
@@ -0,0 +1,22 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/raster.dir/main.cpp.o"
+  "CMakeFiles/raster.dir/src/demosaic.cpp.o"
+  "CMakeFiles/raster.dir/src/desaturate.cpp.o"
+  "CMakeFiles/raster.dir/src/hsv_to_rgb.cpp.o"
+  "CMakeFiles/raster.dir/src/hue_shift.cpp.o"
+  "CMakeFiles/raster.dir/src/over.cpp.o"
+  "CMakeFiles/raster.dir/src/reflect.cpp.o"
+  "CMakeFiles/raster.dir/src/rgb_to_gray.cpp.o"
+  "CMakeFiles/raster.dir/src/rgb_to_hsv.cpp.o"
+  "CMakeFiles/raster.dir/src/rgba_to_rgb.cpp.o"
+  "CMakeFiles/raster.dir/src/rotate.cpp.o"
+  "CMakeFiles/raster.dir/src/simulate_bayer_mosaic.cpp.o"
+  "CMakeFiles/raster.dir/src/write_ppm.cpp.o"
+  "raster"
+  "raster.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/raster.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
